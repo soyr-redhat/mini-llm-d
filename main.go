9c15ecc461029227c4690a6797fc1971e8ff9604
Sawyer Bowerman <sbowerma@redhat.com>
2026-05-17 23:12:51 -0400

    add: update backend routing based on length of input request

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 type ChatMessage struct {
 	Role    string `json:"role"`
 	Content string `json:"content"`
@@ -14,3 +18,25 @@ const (
 	SmallModelPod = "http://localhost:8081" // target for small prompts
 	LargeModelPod = "http://localhost:8082" // target for larger kv cache payloads
 )
+
+/*
+The purpose of this function is to determine what backend to route to based on the following
+ 1. length of input prompt
+*/
+func determineBackend(reqPayload ChatCompletionRequest) string {
+	var totalChars int
+	// Gather length of request
+	for _, msg := range reqPayload.Messages {
+		totalChars += len(msg.Content)
+	}
+
+	fmt.Printf("Model=%s, Total Chars=%d", reqPayload.Model.Model, totalChars)
+
+	// simulation for kv-cache aware routing -> if len prompt > 100 chars, send to hi cap pod
+	if totalChars > 100 {
+		fmt.Println("High-capacity workload detected, routing to LargeModelPod")
+		return LargeModelPod
+	}
+	fmt.Println("Low-Capacity workload detected, routing to SmallModelPod")
+	return SmallModelPod
+}
